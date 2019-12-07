58925f71e29da2dcca7a5a06bff8b1119a189674
tadataku-lab <tadataku.lab@gmail.com>
2019-12-08 01:44:08 +0900

    fix: check attr

--- module/util.py
+++ module/util.py
@@ -9,7 +9,7 @@ from linebot.models import (
 
 def get_profile(event: MessageEvent, api: LineBotApi):
   source = event.source
-  if 'group_id' in source:
+  if hasattr(source, 'group_id'):
     return api.get_group_member_profile(source.group_id, source.user_id)
   else:
     return api.get_profile(source.user_id)
