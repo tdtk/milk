1a9878da8a59d3467b90393c058767b6f1a91cd2
tadataku-lab <tadataku.lab@gmail.com>
2019-12-07 23:05:35 +0900

    fiz show text

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+  print(event.message.text)
   line_bot_api.reply_message(
     event.reply_token,
     TextSendMessage(text=event.message.text))
