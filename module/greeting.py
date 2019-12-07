b67efc7ceba876eb34858fe11218f80a1fcea59f
tadataku-lab <tadataku.lab@gmail.com>
2019-12-08 00:06:34 +0900

    fix: res group member

--- module/greeting.py
+++ module/greeting.py
@@ -5,7 +5,10 @@ from linebot import (
 def is_greeting(text: str):
   return text in ['こんにちは', 'おはよう', 'おやすみ', 'はじめまして']
 
-def greeting(text: str, user_id: str, api: LineBotApi):
-  user_profile = api.get_profile(user_id)
+def greeting(text: str, user_id: str, group_id, api: LineBotApi):
+  if group_id:
+    user_profile = api.get_group_member_profile(group_id, user_id)
+  else:
+    user_profile = api.get_profile(user_id)
   return f'{user_profile.display_name}さん! {text}😊'
 
