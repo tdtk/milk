f683358481337a48583f2bd9fe6e6c45ce51d802
tada <tadataku.lab@gmail.com>
2020-05-04 22:26:33 +0900

    fet: init from str

--- milk/model/spreadsheets/range.py
+++ milk/model/spreadsheets/range.py
@@ -1,3 +1,5 @@
+from milk.util.range_regex import get_range_regex
+
 
 class Range:
   def __init__(self, startRow: int, endRow: int, startColumn: int, endColumn: int, sheet=None):
@@ -10,12 +12,28 @@ class Range:
     self.sheet = sheet
 
   @staticmethod
-  def trancelate_row(row: int):
+  def trancelate_row(row: int) -> str:
     row -= 1
     res = ''
     for i in range((row // 26) + 1):
       res += chr(65 + (row % 26))
     return res
+
+  @staticmethod
+  def retrancelate_row(row: str) -> int:
+    res = 0
+    for i, c in enumerate(row):
+      res += ord(c) - 65 + i * 26
+    return res + 1
+
+  @classmethod
+  def init_from_str(cls, range_: str):
+    m = get_range_regex().match(range_)
+    startRow = cls.retrancelate_row(m.group("startRow"))
+    startColumn = int(m.group("startColumn"))
+    endRow = cls.retrancelate_row(m.group("endRow")) if m.group("endRow") else startRow
+    endColumn = int(m.group("endColumn")) if m.group("endColumn") else startColumn
+    return cls(startRow, endRow, startColumn, endColumn, sheet=m.group("sheet"))
 
   def __str__(self):
     return f"{ self.sheet + '!' if self.sheet else ''}{self.trancelate_row(self.startRow)}{self.startColumn}:{self.trancelate_row(self.endRow)}{self.endColumn}"
--- tests/test_range.py
+++ tests/test_range.py
@@ -7,5 +7,17 @@ class TestRange(unittest.TestCase):
   def test_trancelate_row(self):
     self.assertEqual("A", Range.trancelate_row(1))
     self.assertEqual("AA", Range.trancelate_row(27))
+
+  def test_retrancerate(self):
+    self.assertEqual(1, Range.retrancelate_row("A"))
+    self.assertEqual(27, Range.retrancelate_row("AA"))
+
+  def test_range(self):
     range1 = Range(1, 1, 1, 1, sheet="Sheet1")
     self.assertEqual("Sheet1!A1:A1", str(range1))
+    range2 = Range.init_from_str("Sheet1!A1:A1")
+    self.assertEqual("Sheet1!A1:A1", str(range2))
+    range3 = Range.init_from_str("Sheet1!A1")
+    self.assertEqual("Sheet1!A1:A1", str(range2))
+    range3 = Range.init_from_str("A1")
+    self.assertEqual("A1:A1", str(range3))
