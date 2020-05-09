51feeeeb17f9eada0d212aa2d75ebfda62243461
tada <tadataku.lab@gmail.com>
2020-05-09 19:06:34 +0900

    fix: Pattern type

--- milk/util/range_regex.py
+++ milk/util/range_regex.py
@@ -1,5 +1,5 @@
 import re
 
 
-def get_range_regex() -> re.Pattern:
+def get_range_regex():
   return re.compile(r'(?:(?P<sheet>(?:(?!!)\S)*)!)?(?P<startRow>[a-zA-Z]+)(?P<startColumn>[1-9][0-9]*)(?::(?P<endRow>[a-zA-Z]+)(?P<endColumn>[1-9][0-9]*))?')
