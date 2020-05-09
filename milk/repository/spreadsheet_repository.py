d7e6291777c936ed024054973ddc6cd06da9dcc6
tada <tadataku.lab@gmail.com>
2020-05-10 00:55:43 +0900

    fet: update credential

--- milk/repository/spreadsheet_repository.py
+++ milk/repository/spreadsheet_repository.py
@@ -4,6 +4,7 @@ import os.path
 import json
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
 from google.auth.transport.requests import Request
 from milk.model.spreadsheets.range import Range
 from milk.model.spreadsheets.dimension import Dimension
@@ -15,6 +16,7 @@ from milk.model.spreadsheets.insert_data_option import InsertDataOption
 from milk.model.spreadsheets.append_response import AppendResponse
 from milk.model.spreadsheets.update_values_response import UpdateValuesResponse
 from milk.model.spreadsheets.clear_response import ClearResponse
+import flask
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -27,24 +29,27 @@ class SpreadsheetRepository():
     self.spreadsheet_id = spreadsheet_id
 
   def get_credentials(self):
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens,
-    # and is created automatically when the authorization flow completes for the
-    # first time.
-    if os.path.exists('token.pickle'):
-      with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-      if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-      else:
-        flow = InstalledAppFlow.from_client_config(
-            json.loads(self.credentials), scopes=SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-      pickle.dump(creds, token)
+    # creds = None
+    # # The file token.pickle stores the user's access and refresh tokens,
+    # # and is created automatically when the authorization flow completes for the
+    # # first time.
+    # if os.path.exists('token.pickle'):
+    #   with open('token.pickle', 'rb') as token:
+    #     creds = pickle.load(token)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #   if creds and creds.expired and creds.refresh_token:
+    #     creds.refresh(Request())
+    #   else:
+    #     flow = InstalledAppFlow.from_client_config(
+    #         json.loads(self.credentials), scopes=SCOPES)
+    #     authorization_response = flask.request.url
+    #     flow.fetch_token(authorization_response=authorization_response)
+    #     creds = flow.credentials
+    # # Save the credentials for the next run
+    # with open('token.pickle', 'wb') as token:
+    #   pickle.dump(creds, token)
+    creds = service_account.Credentials.from_service_account_info(json.loads(self.credentials), scopes=SCOPES)
     return creds
 
   def get(
