36b61aef10f65548ca8788b0cb6268d87d15c97a
tada <tadataku.lab@gmail.com>
2020-05-10 12:44:12 +0900

    fix: not datetime it is date

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def handle_postback(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=clear_purchase_data(data["sheet"], int(data["index"]))))
   if data["action"] == "calc_month":
     line_bot_api.reply_message(event.reply_token, TemplateSendMessage(
-        text=get_total_until_date(datetime.date.fromisoformat(event.postback.params["datetime"]))))
+        text=get_total_until_date(datetime.date.fromisoformat(event.postback.params["date"]))))
 
 
 if __name__ == "__main__":
